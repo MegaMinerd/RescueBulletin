ab286585ebbb66748ed5ecc5c05a53097d120d13
MegaMinerd <megaminerd@gmail.com>
2023-03-13 19:10:02 -0400

    Finished layout data

--- src/minerd/relic/dungeon/FloorController.java
+++ src/minerd/relic/dungeon/FloorController.java
@@ -14,17 +14,17 @@ public class FloorController{
 	public ChoiceBox<String> tileset, music, fixedRoom;
 	
 	//Structure tab
-	public TextField connectivity, hallDensity;
+	public TextField roomDensity, connectivity, hallDensity;
 	public ChoiceBox<String> layoutType;
 	public CheckBox hasDeadEnds;
 	
 	//Terrain Settings tab
-	public TextField waterDensity;
+	public TextField terrainRooms, waterDensity;
 	public ChoiceBox<String> weather, visibility;
 	public CheckBox hasPonds, hasExtraTiles;
 	
 	//Items, Traps & Enemies tab
-	public TextField itemDensity, trapDensity, maxCoinAmnt, pokemonDensity;
+	public TextField itemDensity, trapDensity, maxCoinAmnt, pokemonDensity, buriedDensity;
 	
 	//Chances tab
 	public Slider shopChance, houseChance, mazeChance, stickyChance;
@@ -32,14 +32,14 @@ public class FloorController{
 	public void loadLayout() {
 		try {
 			//System.out.println(Integer.toHexString(RomManipulator.getFilePointer()));
-			layoutType.getSelectionModel().select(RomManipulator.readUnsignedShort());
+			layoutType.getSelectionModel().select(RomManipulator.readUnsignedByte());
+			roomDensity.setText(RomManipulator.readUnsignedByte()+"");
 			tileset.getSelectionModel().select(RomManipulator.readUnsignedByte());
 			music.getSelectionModel().select(RomManipulator.readUnsignedByte());
 			weather.getItems().addAll("Clear", "Sunny", "Sandstorm", "Cloudy", "Rainy", "Hail", "Fog", "Snow", "Random");
 			weather.getSelectionModel().select(RomManipulator.readUnsignedByte());
 			connectivity.setText(RomManipulator.readUnsignedByte()+"");
 			pokemonDensity.setText(RomManipulator.readUnsignedByte()+"");
-			RomManipulator.skip(4);
 			shopChance.setValue(RomManipulator.readUnsignedByte()/100.0);
 			houseChance.setValue(RomManipulator.readUnsignedByte()/100.0);
 			mazeChance.setValue(RomManipulator.readUnsignedByte()/100.0);
@@ -52,11 +52,13 @@ public class FloorController{
 			trapDensity.setText(RomManipulator.readUnsignedByte()+"");
 			floorNum.setText("Floor " + RomManipulator.readUnsignedByte());
 			fixedRoom.getSelectionModel().select(RomManipulator.readUnsignedByte());
-			hallDensity.setText(RomManipulator.readUnsignedShort()+"");
+			hallDensity.setText(RomManipulator.readUnsignedByte()+"");
+			terrainRooms.setText(RomManipulator.readUnsignedByte()+"");
 			waterDensity.setText(RomManipulator.readUnsignedByte()+"");
 			visibility.getItems().addAll("Normal", "1 tile", "2 tiles");
 			visibility.getSelectionModel().select(RomManipulator.readUnsignedByte());
 			maxCoinAmnt.setText(RomManipulator.readUnsignedByte()*5+"");
+			buriedDensity.setText(RomManipulator.readUnsignedByte()+"");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
