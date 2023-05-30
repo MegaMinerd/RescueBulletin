45174d7c2308a0476846784a62e46ba9dd6dda90
MegaMinerd <megaminerd@gmail.com>
2023-05-30 11:57:21 -0400

    Mostly implemented SbinFile
    There's some commented out compile errors. After an all-nighter I do not have enough remaining mental capacity to solve them at this time. This class isn't used yet so it won't cause any runtime issues.

--- src/minerd/relic/file/SbinFile.java
+++ src/minerd/relic/file/SbinFile.java
@@ -5,30 +5,102 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 
 public class SbinFile extends RomFile {
-	private HashMap<String, Pointer> contents;
+	private String name;
+	private int offset; // The offset of this sbin within the full rom
+	private HashMap<String, ByteBuffer> contents;
 
-	public SbinFile(ByteBuffer bufferIn) {
+	public SbinFile(ByteBuffer bufferIn, String nameIn, int offsetIn) {
 		super(bufferIn);
+		this.name = nameIn;
+		this.offset = offsetIn;
+
+		contents = new HashMap<String, ByteBuffer>();
 		try{
 			seek(8);
-			long count = readUnsignedInt();
-			for(int i = 0; i<count; i++)
-				contents.put(readString(parsePointer()), parsePointer());
+			int count = (int) readUnsignedInt();
+			seek(24);
+			String[] names = new String[count];
+			Pointer[] pointers = new Pointer[count];
+			// Read the table of contents
+			for(int i = 0; i<count; i++){
+				names[i] = readString(parsePointer());
+				pointers[i] = parsePointer();
+			}
+			// Build the content table in a separate loop
+			// Doing this as a single loop would be complicated due to referencing i+i
+			for(int i = 0; i<count - 1; i++)
+				addSegment(names[i], pointers[i].getOffset(), pointers[i + 1].getOffset());
+			addSegment(names[count], pointers[count].getOffset(), length());
 		} catch(IOException | InvalidPointerException e){
 
 		}
 	}
 
-	// Todo contents should be a buffer collection
-	public ByteBuffer getSection(int size, int... path)
-			throws IOException, IndexOutOfBoundsException, InvalidPointerException {
-		// seek(footer);
-		for(int sectId : path){
-			skip(4*sectId);
-			seek(parsePointer());
+	// Helper function used by constructor to avoid code repetition
+	private void addSegment(String filename, int start, int end) throws IOException {
+		byte[] segment = new byte[start - end];
+		seek(start);
+		read(segment);
+		contents.put(filename, ByteBuffer.wrap(segment));
+	}
+
+	public ByteBuffer save() throws IOException {
+		int size = 32 + 8*contents.size();
+		int[] pointers = new int[contents.size()*2];
+
+		// First pass to poll for changes and tally total size
+		for(String filename : contents.keySet()){
+			// RomFile file = FileSystem.getCachedFile(name + "/" + filename);
+			// ByteBuffer data = file==null ? contents.get(filename) : file.save();
+			// size += Math.ceil((filename.length() + 1)/4.0)*4 + data.length();
 		}
-		byte[] data = new byte[size];
-		// subBuffer.put(0, buffer, getFilePointer(), size);
-		return buffer.slice(getFilePointer(), size);
+
+		// Reason for first pass: capacity assurance
+		if(buffer.capacity()<size)
+			buffer = ByteBuffer.allocate(size);
+
+		// Write header
+		seek(0);
+		writeString("pksdir0");
+		writeUnsignedInt(contents.size());
+		writeUnsignedInt(offset + 0x18);
+		writeString("pksdir0");
+		skip(8*contents.size());
+
+		// Second pass to write filenames
+		int i = 0;
+		for(String filename : contents.keySet()){// Arrays.sort(contents.keySet())){
+			pointers[i] = offset + getFilePointer();
+			i += 2;
+
+			writeString(filename);
+			while(getFilePointer()%4!=0)
+				writeByte((byte) 0);
+		}
+		writeString("pksdir0");
+
+		// Third pass to write data
+		i = 1;
+		for(String filename : contents.keySet()){// Arrays.sort(contents.keySet())){
+			pointers[i] = offset + getFilePointer();
+			i += 2;
+
+			write(contents.get(filename).array());
+		}
+
+		// Write pointers
+		seek(0x18);
+		for(i = 0; i<contents.size()*2; i++)
+			writeUnsignedInt(pointers[i]);
+
+		// Pad any extra
+		while(getFilePointer()<length())
+			writeByte((byte) -1);
+
+		return buffer;
+	}
+
+	public ByteBuffer getSubfile(String name) {
+		return contents.get(name);
 	}
 }
