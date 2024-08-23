904dade2c4bc9e5f1b84fbdf12d3e1f0685ae3d1
MegaMinerd <megaminerd@gmail.com>
2024-08-23 04:14:12 -0400

    Fixing exceptions in constructor

--- src/minerd/relic/file/SbinFile.java
+++ src/minerd/relic/file/SbinFile.java
@@ -6,60 +6,65 @@ import java.util.HashMap;
 
 public class SbinFile extends BufferedDataHandler {
 	private String name;
-	private int offset; // The offset of this sbin within the full rom
+	private int offset; //The offset of this sbin within the full rom
 	private HashMap<String, ByteBuffer> contents;
 
 	public SbinFile(ByteBuffer bufferIn, String nameIn, int offsetIn) {
+		//TODO: Currently using getAll(). Getting a trimmed segment would be better.
 		super(bufferIn);
 		this.name = nameIn;
 		this.offset = offsetIn;
 
 		contents = new HashMap<String, ByteBuffer>();
 		try{
-			seek(8);
+			seek(offset + 8);
 			int count = (int) readUnsignedInt();
-			seek(24);
+			seek(offset + 24);
 			String[] names = new String[count];
 			Pointer[] pointers = new Pointer[count];
-			// Read the table of contents
+			//Read the table of contents
 			for(int i = 0; i<count; i++){
 				names[i] = readString(parsePointer());
 				pointers[i] = parsePointer();
 			}
-			// Build the content table in a separate loop
-			// Doing this as a single loop would be complicated due to referencing i+i
+			//Build the content table in a separate loop
+			//Doing this as a single loop would be complicated due to referencing i+i
 			for(int i = 0; i<count - 1; i++)
 				addSegment(names[i], pointers[i].getOffset(), pointers[i + 1].getOffset());
-			addSegment(names[count], pointers[count].getOffset(), length());
+			//addSegment(names[count-1], pointers[count-1].getOffset(), length());
 		} catch(IOException | InvalidPointerException e){
 
 		}
 	}
 
-	// Helper function used by constructor to avoid code repetition
+	//Helper function used by constructor to avoid code repetition
 	private void addSegment(String filename, int start, int end) throws IOException {
-		byte[] segment = new byte[start - end];
-		seek(start);
-		read(segment);
-		contents.put(filename, ByteBuffer.wrap(segment));
+		try{
+			byte[] segment = new byte[end - start];
+			seek(start);
+			read(segment);
+			contents.put(filename, ByteBuffer.wrap(segment));
+		} catch(NegativeArraySizeException e){
+			//TODO: Find a better end detection method
+		}
 	}
 
 	public ByteBuffer save() throws IOException {
 		int size = 32 + 8*contents.size();
 		int[] pointers = new int[contents.size()*2];
 
-		// First pass to poll for changes and tally total size
+		//First pass to poll for changes and tally total size
 		for(String filename : contents.keySet()){
-			// RomFile file = FileSystem.getCachedFile(name + "/" + filename);
-			// ByteBuffer data = file==null ? contents.get(filename) : file.save();
-			// size += Math.ceil((filename.length() + 1)/4.0)*4 + data.length();
+			//RomFile file = FileSystem.getCachedFile(name + "/" + filename);
+			//ByteBuffer data = file==null ? contents.get(filename) : file.save();
+			//size += Math.ceil((filename.length() + 1)/4.0)*4 + data.length();
 		}
 
-		// Reason for first pass: capacity assurance
+		//Reason for first pass: capacity assurance
 		if(buffer.capacity()<size)
 			buffer = ByteBuffer.allocate(size);
 
-		// Write header
+		//Write header
 		seek(0);
 		writeString("pksdir0");
 		writeUnsignedInt(contents.size());
@@ -67,9 +72,9 @@ public class SbinFile extends BufferedDataHandler {
 		writeString("pksdir0");
 		skip(8*contents.size());
 
-		// Second pass to write filenames
+		//Second pass to write filenames
 		int i = 0;
-		for(String filename : contents.keySet()){// Arrays.sort(contents.keySet())){
+		for(String filename : contents.keySet()){//Arrays.sort(contents.keySet())){
 			pointers[i] = offset + getFilePointer();
 			i += 2;
 
@@ -79,21 +84,21 @@ public class SbinFile extends BufferedDataHandler {
 		}
 		writeString("pksdir0");
 
-		// Third pass to write data
+		//Third pass to write data
 		i = 1;
-		for(String filename : contents.keySet()){// Arrays.sort(contents.keySet())){
+		for(String filename : contents.keySet()){//Arrays.sort(contents.keySet())){
 			pointers[i] = offset + getFilePointer();
 			i += 2;
 
 			write(contents.get(filename).array());
 		}
 
-		// Write pointers
+		//Write pointers
 		seek(0x18);
 		for(i = 0; i<contents.size()*2; i++)
 			writeUnsignedInt(pointers[i]);
 
-		// Pad any extra
+		//Pad any extra
 		while(getFilePointer()<length())
 			writeByte((byte) -1);
 
